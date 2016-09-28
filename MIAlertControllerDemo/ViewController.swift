9278c2eab5795ffea12840e684062601fd18c750
Mario Iannotta <m.iannotta@reply.it>
2016-09-28 15:11:50 +0200

    - Added pod

--- MIAlertControllerDemo/ViewController.swift
+++ MIAlertControllerDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MIAlertController
 
 class ViewController: UIViewController {
     
---
+++ Pods/Target Support Files/MIAlertController/MIAlertController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MIAlertController : NSObject
+@end
+@implementation PodsDummy_MIAlertController
+@end
